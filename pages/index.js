ef460f5f9ee986a68f7b96f481a25be7185c21a7
Gustavo Oliveira <gus.oliveira.nas@gmail.com>
2025-01-03 15:26:33 +0000

    mensagem de incentivo para a Sara

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
+import React from "react";
+
 function Home() {
-    return <h1>Banana e mamão com laranja</h1>
+    return (
+        <div className="letter-container">
+            <h2 className="letter-title">Para Sara, meu amor.</h2>
+            <p className="letter-paragraph"> Sara, minha esposa, meu amor, minha companheira e mãe do meu filho. </p>
+            <p className="letter-paragraph"> Sei que estão sendo dias difíceis para você, e eu entendo. </p>
+            <p className="letter-paragraph"> Você deve se sentir frustrada e cansada, mas as coisas estão prestes a mudar. </p>
+            <p className="letter-paragraph"> A tão sonhada mudança para nossa casa está cada vez mais próxima. </p>
+            <p className="letter-paragraph"> Teremos nosso tão sonhado espaço e você poderá fazer seus bolos e se vestir como quiser sem se preocupar com ninguém. </p>
+            <p className="letter-paragraph"> Nosso filho vai poder brincar mais livremente. </p>
+            <p className="letter-paragraph"> Nós poderemos brincar mais livremente 😏😏😏. </p>
+            <p className="letter-paragraph"> Poderemos arrumar nossas coisas do nosso jeito, no nosso tempo. </p>
+            <p className="letter-paragraph"> Então eu só quero te falar: Vai ficar tudo bem, eu te amo! </p>
+        </div>
+    );
 }
 
 export default Home;
